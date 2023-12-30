6e4d459f375ff739c6b6da33d98277b35a918cb1
hym3242 <40355816+hym3242@users.noreply.github.com>
2023-12-30 19:51:09 +0800

    Create ceol.c

---
+++ ceol.c
@@ -0,0 +1,25 @@
+// ceol is acronym for "color even/odd lines"
+#include <stdio.h>
+//#include <unistd.h>
+
+int main() {
+    int c;
+    int lf_count = 0; // Count of LF characters encountered
+
+    while ((c = getchar()) != EOF) {
+        if (c == 0x0a) { // LF character
+            lf_count++;
+            if (lf_count % 2 == 1) { // Even position
+                printf("\e[48;5;255m");
+            } else { // Odd position
+                printf("\e[0m");
+            }
+        }
+        putchar(c);
+    }
+
+    // Reset terminal color before exiting
+    printf("\e[0m");
+    //if(lf_count%2==1)write(1,"\n\n",2);
+    return 0;
+}
